eda9feea664a3904aaa9f19e23fb18b6e88f1e9f
niix <njustice@gmail.com>
2012-11-21 12:12:09 -0500

    Log In/Out works

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
+describe "Authentication" do
 
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_selector('input',    text: 'Sign In') }
+  end
 end
